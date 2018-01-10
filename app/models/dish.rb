d0e62a2ed4384f68635bfbe9b215ac0a41ed3759
Raghu Betina <raghu@firstdraft.com>
2018-01-10 23:14:28 +0000

    Direct association between type_of_cuisines and dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,10 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  belongs_to :type_of_food,
+             :class_name => "TypeOfCuisine",
+             :foreign_key => "type_of_cuisine_id"
+
   has_many   :bookmarks,
              :dependent => :destroy
 
--- app/models/type_of_cuisine.rb
+++ app/models/type_of_cuisine.rb
@@ -1,6 +1,9 @@
 class TypeOfCuisine < ApplicationRecord
   # Direct associations
 
+  has_many   :dishes,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
