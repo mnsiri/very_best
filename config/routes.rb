b414b39a391fd2e97dbe3f4aae6b3f693b0eef09
Raghu Betina <raghu@firstdraft.com>
2018-01-10 23:14:17 +0000

    Generate bookmarks resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Bookmark resource:
+  # CREATE
+  get "/bookmarks/new", :controller => "bookmarks", :action => "new"
+  post "/create_bookmark", :controller => "bookmarks", :action => "create"
+
+  # READ
+  get "/bookmarks", :controller => "bookmarks", :action => "index"
+  get "/bookmarks/:id", :controller => "bookmarks", :action => "show"
+
+  # UPDATE
+  get "/bookmarks/:id/edit", :controller => "bookmarks", :action => "edit"
+  post "/update_bookmark/:id", :controller => "bookmarks", :action => "update"
+
+  # DELETE
+  get "/delete_bookmark/:id", :controller => "bookmarks", :action => "destroy"
+  #------------------------------
+
   # Routes for the Type_of_cuisine resource:
   # CREATE
   get "/type_of_cuisines/new", :controller => "type_of_cuisines", :action => "new"
